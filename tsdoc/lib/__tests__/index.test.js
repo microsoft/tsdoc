87bdee9887f66c5b5ff59c4c3ca6519a3df10cc2
pgonzal <pgonzal@users.noreply.github.com>
2018-05-24 18:28:28 -0700

    Delete accidentally added files

--- tsdoc/lib/__tests__/index.test.js
+++
@@ -1,4 +0,0 @@
-it('Example unit test', function () {
-    expect(1 + 1).toBe(2);
-});
-//# sourceMappingURL=index.test.js.map
--- tsdoc/lib/index.js
+++
@@ -1,2 +0,0 @@
-console.log('Hello, world!');
-//# sourceMappingURL=index.js.map
