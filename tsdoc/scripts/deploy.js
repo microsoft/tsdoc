3416bb35d9d0ff63fb48ceec79884bedf2c4d302
pgonzal <pgonzal@users.noreply.github.com>
2018-06-26 20:02:21 -0700

    Remove file that was accidentally picked up by Git

--- tsdoc/scripts/deploy.js
+++
@@ -1,4 +0,0 @@
-console.log('Executing common/scripts/deploy.js');
-console.log('ARGV: ' + JSON.stringify(process.argv));
-console.log('CWD: ' + process.cwd());
-console.log('NPM_INITCWD: ' + process.env['INIT_CWD']);
