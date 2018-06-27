54b182f7103f58c6a926cfd802f1d804cf7eeef7
pgonzal <pgonzal@users.noreply.github.com>
2018-06-26 20:44:08 -0700

    Redesigned the Tokenizer Jest snapshot representation to be more readable

--- tsdoc/src/__tests__/Tokenizer.test.ts
+++ tsdoc/src/__tests__/Tokenizer.test.ts
@@ -2,7 +2,15 @@ import { TSDocParser, DocComment } from '../index';
 import { Tokenizer, Token, TokenKind } from '../api/Tokenizer';
 
 function escape(s: string): string {
-  return s.replace(/\n/g, '[n]');
+  return s.replace(/\n/g, '[n]')
+    .replace(/\\/g, '[b]');
+}
+
+interface ISnapshotItem {
+  indexOfLine: number;
+  line: string;
+  span: string;
+  tokenKind: string;
 }
 
 function matchSnapshot(buffer: string): void {
@@ -10,27 +18,60 @@ function matchSnapshot(buffer: string): void {
   const docComment: DocComment = tsdocParser.parseString(buffer);
   const tokenizer: Tokenizer = new Tokenizer(docComment.lines);
 
-  const tokens: Token[] = [];
+  const items: ISnapshotItem[] = [];
+  const paddedSpace: string[]  = [ '',   ' ',  '  ',  '   ',  '    ' ];
+  const paddedLArrow: string[] = [ '',   '>',  ' >',  '  >',  '   >' ];
+  const paddedRArrow: string[] = [ '',   '<',  '< ',  '<  ',  '<   ' ];
 
   while (true) {
     const token: Token = tokenizer.getToken();
+
+    let span: string = '';
+    if (token.line.end > 0) {
+      let i: number = token.line.pos - 1;
+      while (i < token.range.pos - 1) {
+        span += paddedSpace[escape(buffer[i]).length];
+        ++i;
+      }
+      span += paddedLArrow[escape(buffer[i]).length];
+      ++i;
+      while (i < token.range.end) {
+        span += paddedSpace[escape(buffer[i]).length];
+        ++i;
+      }
+      if (i === token.line.end) {
+        span += '<';
+      } else {
+        span += paddedRArrow[escape(buffer[i]).length];
+        ++i;
+        while (i < token.line.end) {
+          span += paddedSpace[escape(buffer[i]).length];
+          ++i;
+        }
+
+      }
+    }
+
+    items.push({
+      indexOfLine: docComment.lines.indexOf(token.line),
+      line: '>' + escape(token.line.toString()) + '<',
+      span: span,
+      tokenKind: TokenKind[token.kind]
+    });
+
     if (token.kind === TokenKind.EndOfInput) {
       break;
     }
-    tokens.push(token);
   }
 
   // Assert that getToken() should return EndOfInput repeatedly
   expect(tokenizer.getToken().kind).toEqual(TokenKind.EndOfInput);
   expect(tokenizer.getToken().kind).toEqual(TokenKind.EndOfInput);
 
-  // Generate a snapshot of the token list
-  expect(tokens.map(token => {
-    return {
-      tokenKind: TokenKind[token.kind],
-      range: escape(token.range.getDebugDump('|', '|'))
-    };
-  })).toMatchSnapshot();
+  expect({
+    buffer: escape(buffer),
+    tokens: items
+  }).toMatchSnapshot();
 }
 
 test('00 Tokenizer simple case', () => {
