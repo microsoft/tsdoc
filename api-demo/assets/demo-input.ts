bf4eca7efb6cd4d4660315519f6aea7617a5abbd
pgonzal <pgonzal@users.noreply.github.com>
2018-08-16 17:44:05 -0700

    Move the api-demo input into an external text file

---
+++ api-demo/assets/demo-input.ts
@@ -0,0 +1,14 @@
+
+  /**
+   * Adds two numbers together.
+   *
+   * @remarks
+   * This method is part of the {@link core-libary/Math | Math subsystem}.
+   * This incomplete HTML tag should be reported as an error: <tag
+   *
+   * @param x - The first number to add
+   * @param y - The second number to add
+   * @returns The sum of `x` and `y`
+   *
+   * @beta
+   */
--- api-demo/src/start.ts
+++ api-demo/src/start.ts
@@ -1,30 +1,19 @@
-import * as os from 'os';
 import * as colors from 'colors';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as os from 'os';
 import { TSDocParser, ParserContext, DocComment } from '@microsoft/tsdoc';
 import { Formatter } from './Formatter';
 
-console.log(colors.cyan('*** TSDoc API demo ***'));
+console.log(colors.cyan('*** TSDoc API demo ***') + os.EOL);
 
+const inputFilename: string = path.resolve(path.join(__dirname, '..', 'assets', 'demo-input.ts'));
+console.log('Reading assets/demo-input.ts...');
+
+const inputBuffer: string = fs.readFileSync(inputFilename).toString();
+
+// NOTE: Optionally, can provide a TSDocParserConfiguration here
 const tsdocParser: TSDocParser = new TSDocParser();
-
-const inputBuffer: string = [
-  /* line  1 */ ' ',
-  /* line  2 */ '  /**',
-  /* line  3 */ '   * Adds two numbers together.',
-  /* line  4 */ '   *',
-  /* line  5 */ '   * @remarks',
-  /* line  6 */ '   * This method is part of the {@link core-libary/Math | Math subsystem}.',
-  /* line  7 */ '   * This HTML tag has an error: <tag',
-  /* line  7 */ '   *',
-  /* line  8 */ '   * @param x - The first number to add',
-  /* line  9 */ '   * @param y - The second number to add',
-  /* line  0 */ '   * @returns The sum of `x` and `y`',
-  /* line 11 */ '   *',
-  /* line 12 */ '   * @beta',
-  /* line 13 */ '   */',
-  /* line 14 */ '            '
-].join('\n');
-
 const parserContext: ParserContext = tsdocParser.parseString(inputBuffer);
 
 console.log(os.EOL + colors.green('Input Buffer:') + os.EOL);
@@ -41,7 +30,7 @@ if (parserContext.log.messages.length === 0) {
   console.log('No errors or warnings.');
 } else {
   for (const message of parserContext.log.messages.map(x => x.toString())) {
-    console.log(message);
+    console.log(inputFilename + message);
   }
 }
 
