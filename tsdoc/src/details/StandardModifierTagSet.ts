7ce262657e141cf28ab501d8c251796197f049ed
pgonzal <pgonzal@users.noreply.github.com>
2018-08-23 16:04:24 -0700

    Add new tags to StandardModifierTagSet.ts

--- tsdoc/src/details/StandardModifierTagSet.ts
+++ tsdoc/src/details/StandardModifierTagSet.ts
@@ -9,35 +9,84 @@ export class StandardModifierTagSet extends ModifierTagSet {
   /**
    * Returns true if the `@alpha` modifier tag was specified.
    */
-  public get isAlpha(): boolean {
+  public isAlpha(): boolean {
     return this.hasModifierTagWithUpperCase(StandardTags.alpha.tagNameWithUpperCase);
   }
 
   /**
    * Returns true if the `@beta` modifier tag was specified.
    */
-  public get isBeta(): boolean {
+  public isBeta(): boolean {
     return this.hasModifierTagWithUpperCase(StandardTags.beta.tagNameWithUpperCase);
+  }
+
+  /**
+   * Returns true if the `@eventProperty` modifier tag was specified.
+   */
+  public isEventProperty(): boolean {
+    return this.hasModifierTagWithUpperCase(StandardTags.eventProperty.tagNameWithUpperCase);
   }
 
   /**
    * Returns true if the `@experimental` modifier tag was specified.
    */
-  public get isExperimental(): boolean {
+  public isExperimental(): boolean {
     return this.hasModifierTagWithUpperCase(StandardTags.experimental.tagNameWithUpperCase);
   }
 
   /**
    * Returns true if the `@internal` modifier tag was specified.
    */
-  public get isInternal(): boolean {
+  public isInternal(): boolean {
     return this.hasModifierTagWithUpperCase(StandardTags.internal.tagNameWithUpperCase);
+  }
+
+  /**
+   * Returns true if the `@override` modifier tag was specified.
+   */
+  public isOverride(): boolean {
+    return this.hasModifierTagWithUpperCase(StandardTags.override.tagNameWithUpperCase);
+  }
+
+  /**
+   * Returns true if the `@packageDocumentation` modifier tag was specified.
+   */
+  public isPackageDocumentation(): boolean {
+    return this.hasModifierTagWithUpperCase(StandardTags.packageDocumentation.tagNameWithUpperCase);
+  }
+
+  /**
+   * Returns true if the `@preapproved` modifier tag was specified.
+   */
+  public isPreapproved(): boolean {
+    return this.hasModifierTagWithUpperCase(StandardTags.preapproved.tagNameWithUpperCase);
+  }
+
+  /**
+   * Returns true if the `@public` modifier tag was specified.
+   */
+  public isPublic(): boolean {
+    return this.hasModifierTagWithUpperCase(StandardTags.public.tagNameWithUpperCase);
   }
 
   /**
    * Returns true if the `@readonly` modifier tag was specified.
    */
-  public get isReadonly(): boolean {
+  public isReadonly(): boolean {
     return this.hasModifierTagWithUpperCase(StandardTags.readonly.tagNameWithUpperCase);
+  }
+
+  /**
+   * Returns true if the `@sealed` modifier tag was specified.
+   */
+  public isSealed(): boolean {
+    return this.hasModifierTagWithUpperCase(StandardTags.sealed.tagNameWithUpperCase);
+  }
+
+  /**
+   * Returns true if the `@virtual` modifier tag was specified.
+   */
+  public isVirtual(): boolean {
+    return this.hasModifierTagWithUpperCase(StandardTags.virtual.tagNameWithUpperCase);
   }
 }
