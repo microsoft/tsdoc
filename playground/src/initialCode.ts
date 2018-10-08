7c3cc77174c1d0549fafb3bc39518eb3d05d6b09
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2018-10-08 01:56:34 -0700

    Extracting initial code into a TS file.

---
+++ playground/src/initialCode.ts
@@ -0,0 +1,12 @@
+/**
+ * Returns the average of two numbers.
+ *
+ * @remarks
+ * This method is part of the {@link core-library#Statistics | Statistics subsystem}.
+ *
+ * @param x - The first input number
+ * @param y - The second input number
+ * @returns The arithmetic mean of `x` and `y`
+ *
+ * @beta
+ */
