ef158b9df919b6ad58b89e8d03258e13ac352fef
Kevin Coughlin <keco@microsoft.com>
2018-10-09 15:49:13 -0700

    Fixes a11y error due to missing Errors: label for ErrorsPane

--- playground/src/PlaygroundView.tsx
+++ playground/src/PlaygroundView.tsx
@@ -229,10 +229,10 @@ export class PlaygroundView extends React.Component<IPlaygroundViewProps, IPlayg
 
     return (
       <>
-        Errors:
-        <br />
+        <label htmlFor="errors">Errors:</label>
         <FlexColDiv style={boxStyle}>
           <textarea
+            id="errors"
             className='playground-errors-textarea'
             readOnly={ true }
             value={ errorsText }
