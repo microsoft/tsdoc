309a813272cefed0db86d6cb9b2617194e1ba0c5
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2018-10-09 21:30:08 -0700

    Introducing vs-dark tsdoc colors.

--- playground/src/SyntaxStyler/DocNodeSyntaxStylerTheme.ts
+++ playground/src/SyntaxStyler/DocNodeSyntaxStylerTheme.ts
@@ -26,3 +26,21 @@ export const vs: IDocNodeSyntaxStylerTheme = {
   'tsdoc.error': { foreground: 'FFFFFF', background: 'c00000' },
   'tsdoc.escaped': { background: 'e0e0e0' }
 };
+
+export const vsDark: IDocNodeSyntaxStylerTheme = {
+  'tsdoc.delimiter': { foreground: 'a6a6a6' },
+  'tsdoc.tag.block': { foreground: '569cd6', fontWeight: 'bold', className: 'tsdoc-blocktag' },
+  'tsdoc.tag.inline': { foreground: '569cd6', fontWeight: 'bold' },
+  'tsdoc.tag.modifier': { foreground: 'b46695', fontWeight: 'bold' },
+  'tsdoc.tag.undefined': { foreground: 'a6a6a6' },
+  'tsdoc.member.selector': { foreground: 'b46695' },
+  'tsdoc.member.identifier': { foreground: '5b9bd5' },
+  'tsdoc.element.name': { foreground: '569cd6' },
+  'tsdoc.element.attribute.name': { foreground: '569cd6' },
+  'tsdoc.element.attribute.value': { foreground: 'd4d4d4' },
+  'tsdoc.url': { foreground: '5b9bd5', className: 'tsdoc-underline' },
+  'tsdoc.code': { foreground: 'd4d4d4' },
+  'tsdoc.language': { foreground: 'b46695' },
+  'tsdoc.error': { foreground: 'd4d4d4', background: 'c00000' },
+  'tsdoc.escaped': { background: '333333' }
+};
