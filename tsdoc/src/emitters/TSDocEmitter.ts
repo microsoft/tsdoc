ad9e78094b7a45da0de64a9bc890a7738c5ed94f
pgonzal <pgonzal@users.noreply.github.com>
2018-10-25 00:24:06 -0700

    Convert "output" to a class member

--- tsdoc/src/emitters/TSDocEmitter.ts
+++ tsdoc/src/emitters/TSDocEmitter.ts
@@ -34,6 +34,13 @@ enum LineState {
 }
 
 /**
+ * Constructor options for {@link TSDocEmitter}.
+ */
+export interface ITSDocEmitterParameters {
+
+}
+
+/**
  * Renders a DocNode tree as a code comment.
  */
 export class TSDocEmitter {
@@ -41,17 +48,19 @@ export class TSDocEmitter {
 
   private _lineState: LineState = LineState.Closed;
   private _previousLineHadContent: boolean = false;
+  private _output: StringBuilder | undefined;
 
   public renderComment(output: StringBuilder, docComment: DocComment): void {
     this._lineState = LineState.Closed;
     this._previousLineHadContent = false;
+    this._output = output;
 
-    this._renderNode(output, docComment);
+    this._renderNode(docComment);
 
-    this._writeEnd(output);
+    this._writeEnd();
   }
 
-  private _renderNode(output: StringBuilder, docNode: DocNode | undefined): void {
+  private _renderNode(docNode: DocNode | undefined): void {
     if (docNode === undefined) {
       return;
     }
@@ -59,29 +68,29 @@ export class TSDocEmitter {
     switch (docNode.kind) {
       case DocNodeKind.Block:
         const docBlock: DocBlock = docNode as DocBlock;
-        this._writeNewline(output);
-        this._renderNode(output, docBlock.blockTag);
-        this._renderNode(output, docBlock.content);
+        this._writeNewline();
+        this._renderNode(docBlock.blockTag);
+        this._renderNode(docBlock.content);
         break;
 
       case DocNodeKind.BlockTag:
         const docBlockTag: DocBlockTag = docNode as DocBlockTag;
         if (this._lineState === LineState.MiddleOfLine) {
-          this._writeContent(output, ' ');
+          this._writeContent(' ');
         }
-        this._writeContent(output, docBlockTag.tagName);
+        this._writeContent(docBlockTag.tagName);
         break;
 
       case DocNodeKind.CodeSpan:
         const docCodeSpan: DocCodeSpan = docNode as DocCodeSpan;
-        this._writeContent(output, '`');
-        this._writeContent(output, docCodeSpan.code);
-        this._writeContent(output, '`');
+        this._writeContent('`');
+        this._writeContent(docCodeSpan.code);
+        this._writeContent('`');
         break;
 
       case DocNodeKind.Comment:
         const docComment: DocComment = docNode as DocComment;
-        this._renderNodes(output, [
+        this._renderNodes([
           docComment.summarySection,
           docComment.remarksBlock,
           docComment.privateRemarks,
@@ -93,115 +102,115 @@ export class TSDocEmitter {
           docComment.inheritDocTag
         ]);
         if (docComment.modifierTagSet.nodes.length > 0) {
-          this._ensureLineSkipped(output);
-          this._renderNodes(output, docComment.modifierTagSet.nodes);
+          this._ensureLineSkipped();
+          this._renderNodes(docComment.modifierTagSet.nodes);
         }
         break;
 
       case DocNodeKind.DeclarationReference:
         const docDeclarationReference: DocDeclarationReference = docNode as DocDeclarationReference;
-        this._writeContent(output, docDeclarationReference.packageName);
-        this._writeContent(output, docDeclarationReference.importPath);
+        this._writeContent(docDeclarationReference.packageName);
+        this._writeContent(docDeclarationReference.importPath);
         if (docDeclarationReference.packageName !== undefined || docDeclarationReference.importPath !== undefined) {
-          this._writeContent(output, '#');
+          this._writeContent('#');
         }
-        this._renderNodes(output, docDeclarationReference.memberReferences);
+        this._renderNodes(docDeclarationReference.memberReferences);
         break;
 
       case DocNodeKind.ErrorText:
         const docErrorText: DocErrorText = docNode as DocErrorText;
-        this._writeContent(output, docErrorText.text);
+        this._writeContent(docErrorText.text);
         break;
 
       case DocNodeKind.EscapedText:
         const docEscapedText: DocEscapedText = docNode as DocEscapedText;
-        this._writeContent(output, docEscapedText.encodedText);
+        this._writeContent(docEscapedText.encodedText);
         break;
 
       case DocNodeKind.FencedCode:
         const docFencedCode: DocFencedCode = docNode as DocFencedCode;
 
-        this._ensureAtStartOfLine(output);
+        this._ensureAtStartOfLine();
 
-        this._writeContent(output, '```');
-        this._writeContent(output, docFencedCode.language);
-        this._writeNewline(output);
-        this._writeContent(output, docFencedCode.code);
-        this._writeContent(output, '```');
-        this._writeNewline(output);
-        this._writeNewline(output);
+        this._writeContent('```');
+        this._writeContent(docFencedCode.language);
+        this._writeNewline();
+        this._writeContent(docFencedCode.code);
+        this._writeContent('```');
+        this._writeNewline();
+        this._writeNewline();
         break;
 
       case DocNodeKind.HtmlAttribute:
         const docHtmlAttribute: DocHtmlAttribute = docNode as DocHtmlAttribute;
-        this._writeContent(output, docHtmlAttribute.name);
-        this._writeContent(output, docHtmlAttribute.spacingAfterName);
-        this._writeContent(output, '=');
-        this._writeContent(output, docHtmlAttribute.spacingAfterEquals);
-        this._writeContent(output, docHtmlAttribute.value);
-        this._writeContent(output, docHtmlAttribute.spacingAfterValue);
+        this._writeContent(docHtmlAttribute.name);
+        this._writeContent(docHtmlAttribute.spacingAfterName);
+        this._writeContent('=');
+        this._writeContent(docHtmlAttribute.spacingAfterEquals);
+        this._writeContent(docHtmlAttribute.value);
+        this._writeContent(docHtmlAttribute.spacingAfterValue);
         break;
 
       case DocNodeKind.HtmlEndTag:
         const docHtmlEndTag: DocHtmlEndTag = docNode as DocHtmlEndTag;
-        this._writeContent(output, '</');
-        this._writeContent(output, docHtmlEndTag.name);
-        this._writeContent(output, '>');
+        this._writeContent('</');
+        this._writeContent(docHtmlEndTag.name);
+        this._writeContent('>');
         break;
 
       case DocNodeKind.HtmlStartTag:
         const docHtmlStartTag: DocHtmlStartTag = docNode as DocHtmlStartTag;
-        this._writeContent(output, '<');
-        this._writeContent(output, docHtmlStartTag.name);
-        this._writeContent(output, docHtmlStartTag.spacingAfterName);
+        this._writeContent('<');
+        this._writeContent(docHtmlStartTag.name);
+        this._writeContent(docHtmlStartTag.spacingAfterName);
 
         let needsSpace: boolean = docHtmlStartTag.spacingAfterName === undefined
           || docHtmlStartTag.spacingAfterName.length === 0;
 
         for (const attribute of docHtmlStartTag.htmlAttributes) {
           if (needsSpace) {
-            this._writeContent(output, ' ');
+            this._writeContent(' ');
           }
-          this._renderNode(output, attribute);
+          this._renderNode(attribute);
           needsSpace = attribute.spacingAfterValue === undefined || attribute.spacingAfterValue.length === 0;
         }
-        this._writeContent(output, docHtmlStartTag.selfClosingTag ? '/>' : '>');
+        this._writeContent(docHtmlStartTag.selfClosingTag ? '/>' : '>');
         break;
 
       case DocNodeKind.InheritDocTag:
         const docInheritDocTag: DocInheritDocTag = docNode as DocInheritDocTag;
-        this._renderInlineTag(output, docInheritDocTag, () => {
+        this._renderInlineTag(docInheritDocTag, () => {
           if (docInheritDocTag.declarationReference) {
-            this._writeContent(output, ' ');
-            this._renderNode(output, docInheritDocTag.declarationReference);
+            this._writeContent(' ');
+            this._renderNode(docInheritDocTag.declarationReference);
           }
         });
         break;
 
       case DocNodeKind.InlineTag:
         const docInlineTag: DocInlineTag = docNode as DocInlineTag;
-        this._renderInlineTag(output, docInlineTag, () => {
+        this._renderInlineTag(docInlineTag, () => {
           if (docInlineTag.tagContent.length > 0) {
-            this._writeContent(output, ' ');
-            this._writeContent(output, docInlineTag.tagContent);
+            this._writeContent(' ');
+            this._writeContent(docInlineTag.tagContent);
           }
         });
         break;
 
       case DocNodeKind.LinkTag:
         const docLinkTag: DocLinkTag = docNode as DocLinkTag;
-        this._renderInlineTag(output, docLinkTag, () => {
+        this._renderInlineTag(docLinkTag, () => {
           if (docLinkTag.urlDestination !== undefined || docLinkTag.codeDestination !== undefined) {
-            this._writeContent(output, ' ');
+            this._writeContent(' ');
             if (docLinkTag.urlDestination !== undefined) {
-              this._writeContent(output, docLinkTag.urlDestination);
+              this._writeContent(docLinkTag.urlDestination);
             } else {
-              this._renderNode(output, docLinkTag.codeDestination);
+              this._renderNode(docLinkTag.codeDestination);
             }
           }
           if (docLinkTag.linkText !== undefined) {
-            this._writeContent(output, '|');
-            this._writeContent(output, docLinkTag.linkText);
+            this._writeContent('|');
+            this._writeContent(docLinkTag.linkText);
           }
         });
         break;
@@ -209,99 +218,99 @@ export class TSDocEmitter {
       case DocNodeKind.MemberIdentifier:
         const docMemberIdentifier: DocMemberIdentifier = docNode as DocMemberIdentifier;
         if (docMemberIdentifier.hasQuotes) {
-          this._writeContent(output, '"');
-          this._writeContent(output, docMemberIdentifier.identifier); // todo: encoding
-          this._writeContent(output, '"');
+          this._writeContent('"');
+          this._writeContent(docMemberIdentifier.identifier); // todo: encoding
+          this._writeContent('"');
         } else {
-          this._writeContent(output, docMemberIdentifier.identifier);
+          this._writeContent(docMemberIdentifier.identifier);
         }
         break;
 
       case DocNodeKind.MemberReference:
         const docMemberReference: DocMemberReference = docNode as DocMemberReference;
         if (docMemberReference.hasDot) {
-          this._writeContent(output, '.');
+          this._writeContent('.');
         }
 
         if (docMemberReference.selector) {
-          this._writeContent(output, '(');
+          this._writeContent('(');
         }
 
         if (docMemberReference.memberSymbol) {
-          this._renderNode(output, docMemberReference.memberSymbol);
+          this._renderNode(docMemberReference.memberSymbol);
         } else {
-          this._renderNode(output, docMemberReference.memberIdentifier);
+          this._renderNode(docMemberReference.memberIdentifier);
         }
 
         if (docMemberReference.selector) {
-          this._writeContent(output, ':');
-          this._renderNode(output, docMemberReference.selector);
-          this._writeContent(output, ')');
+          this._writeContent(':');
+          this._renderNode(docMemberReference.selector);
+          this._writeContent(')');
         }
         break;
 
       case DocNodeKind.MemberSelector:
         const docMemberSelector: DocMemberSelector = docNode as DocMemberSelector;
-        this._writeContent(output, docMemberSelector.selector);
+        this._writeContent(docMemberSelector.selector);
         break;
 
       case DocNodeKind.MemberSymbol:
         const docMemberSymbol: DocMemberSymbol = docNode as DocMemberSymbol;
-        this._writeContent(output, '[');
-        this._renderNode(output, docMemberSymbol.symbolReference);
-        this._writeContent(output, ']');
+        this._writeContent('[');
+        this._renderNode(docMemberSymbol.symbolReference);
+        this._writeContent(']');
         break;
 
       case DocNodeKind.Section:
         const docSection: DocSection = docNode as DocSection;
-        this._renderNodes(output, docSection.nodes);
+        this._renderNodes(docSection.nodes);
         break;
 
       case DocNodeKind.Paragraph:
         const trimmedParagraph: DocParagraph = DocNodeTransforms.trimSpacesInParagraph(docNode as DocParagraph);
         if (trimmedParagraph.nodes.length > 0) {
-          this._ensureLineSkipped(output);
-          this._renderNodes(output, trimmedParagraph.nodes);
-          this._writeNewline(output);
+          this._ensureLineSkipped();
+          this._renderNodes(trimmedParagraph.nodes);
+          this._writeNewline();
         }
         break;
 
       case DocNodeKind.PlainText:
         const docPlainText: DocPlainText = docNode as DocPlainText;
-        this._writeContent(output, docPlainText.text);
+        this._writeContent(docPlainText.text);
         break;
     }
   }
 
-  private _renderInlineTag(output: StringBuilder, docInlineTagBase: DocInlineTagBase,
+  private _renderInlineTag(docInlineTagBase: DocInlineTagBase,
     writeInlineTagContent: () => void): void {
 
-    this._writeContent(output, '{');
-    this._writeContent(output, docInlineTagBase.tagName);
+    this._writeContent('{');
+    this._writeContent(docInlineTagBase.tagName);
     writeInlineTagContent();
-    this._writeContent(output, '}');
+    this._writeContent('}');
   }
 
-  private _renderNodes(output: StringBuilder, docNodes: ReadonlyArray<DocNode | undefined>): void {
+  private _renderNodes(docNodes: ReadonlyArray<DocNode | undefined>): void {
     for (const docNode of docNodes) {
-      this._renderNode(output, docNode);
+      this._renderNode(docNode);
     }
   }
 
-  private _ensureAtStartOfLine(output: StringBuilder): void {
+  private _ensureAtStartOfLine(): void {
     if (this._lineState === LineState.MiddleOfLine) {
-      this._writeNewline(output);
+      this._writeNewline();
     }
   }
 
-  private _ensureLineSkipped(output: StringBuilder): void {
-    this._ensureAtStartOfLine(output);
+  private _ensureLineSkipped(): void {
+    this._ensureAtStartOfLine();
     if (this._previousLineHadContent) {
-      this._writeNewline(output);
+      this._writeNewline();
     }
   }
 
-  private _writeContent(output: StringBuilder, content: string | undefined): void {
+  private _writeContent(content: string | undefined): void {
     if (content === undefined || content.length === 0) {
       return;
     }
@@ -313,48 +322,48 @@ export class TSDocEmitter {
         if (firstLine) {
           firstLine = false;
         } else {
-          this._writeNewline(output);
+          this._writeNewline();
         }
-        this._writeContent(output, line);
+        this._writeContent(line);
       }
       return;
     }
 
     if (this._lineState === LineState.Closed) {
-      output.append('/**' + this.eol
+      this._output!.append('/**' + this.eol
         + ' *');
       this._lineState = LineState.StartOfLine;
     }
 
     if (this._lineState === LineState.StartOfLine) {
-      output.append(' ');
+      this._output!.append(' ');
     }
 
-    output.append(content);
+    this._output!.append(content);
     this._lineState = LineState.MiddleOfLine;
     this._previousLineHadContent = true;
   }
 
-  private _writeNewline(output: StringBuilder): void {
+  private _writeNewline(): void {
     if (this._lineState === LineState.Closed) {
-      output.append('/**' + this.eol
+      this._output!.append('/**' + this.eol
         + ' *');
       this._lineState = LineState.StartOfLine;
     }
 
     this._previousLineHadContent = this._lineState === LineState.MiddleOfLine;
 
-    output.append(this.eol + ' *');
+    this._output!.append(this.eol + ' *');
     this._lineState = LineState.StartOfLine;
   }
 
-  private _writeEnd(output: StringBuilder): void {
+  private _writeEnd(): void {
     if (this._lineState === LineState.MiddleOfLine) {
-      this._writeNewline(output);
+      this._writeNewline();
     }
 
     if (this._lineState !== LineState.Closed) {
-      output.append('/' + this.eol);
+      this._output!.append('/' + this.eol);
       this._lineState = LineState.Closed;
     }
   }
