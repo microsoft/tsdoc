7db88effcbac5f24bf72d9688cd40b15005f97d7
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2019-11-15 01:34:09 -0800

    Implement TSDocConfigFile state

--- tsdoc-config/src/TSDocConfigFile.ts
+++ tsdoc-config/src/TSDocConfigFile.ts
@@ -1,10 +1,7 @@
+import { TSDocTagDefinition, TSDocTagSyntaxKind } from '@microsoft/tsdoc';
 import * as fs from 'fs';
 import * as path from 'path';
 import * as Ajv from 'ajv';
-
-interface ITSDocConfigFileData {
-  filePath: string;
-}
 
 const ajv: Ajv.Ajv = new Ajv({ verbose: true });
 
@@ -21,15 +18,52 @@ function initializeSchemaValidator(): Ajv.ValidateFunction {
 // to read the properties before a subsequent call may occur.
 const tsdocSchemaValidator: Ajv.ValidateFunction = initializeSchemaValidator();
 
+interface ITagConfigJson {
+  tagName: string;
+  syntaxKind: 'inline' | 'block' | 'modifier';
+  allowMultiple?: boolean;
+}
+
 interface IConfigJson {
+  tsdocVersion: string;
+  extends?: string[];
+  tagDefinitions: ITagConfigJson[];
 }
 
 export class TSDocConfigFile {
 
   public readonly filePath: string;
 
-  private constructor(data: ITSDocConfigFileData) {
-    this.filePath = data.filePath;
+  public readonly tsdocVersion: string;
+
+  public readonly extends: ReadonlyArray<string>;
+
+  public readonly tagDefinitions: ReadonlyArray<TSDocTagDefinition>;
+
+  private constructor(filePath: string, configJson: IConfigJson) {
+    this.filePath = filePath;
+    this.tsdocVersion = configJson.tsdocVersion;
+    this.extends = configJson.extends || [];
+    const tagDefinitions: TSDocTagDefinition[] = [];
+
+    for (const jsonTagDefinition of configJson.tagDefinitions || []) {
+      let syntaxKind: TSDocTagSyntaxKind;
+      switch (jsonTagDefinition.syntaxKind) {
+        case 'inline': syntaxKind = TSDocTagSyntaxKind.InlineTag; break;
+        case 'block': syntaxKind = TSDocTagSyntaxKind.BlockTag; break;
+        case 'modifier': syntaxKind = TSDocTagSyntaxKind.ModifierTag; break;
+        default:
+          // The JSON schema should have caught this error
+          throw new Error('Unexpected tag kind');
+      }
+      tagDefinitions.push(new TSDocTagDefinition({
+        tagName: jsonTagDefinition.tagName,
+        syntaxKind: syntaxKind,
+        allowMultiple: jsonTagDefinition.allowMultiple
+      }));
+    }
+
+    this.tagDefinitions = tagDefinitions;
   }
 
   public static load(jsonFilePath: string): TSDocConfigFile {
@@ -46,8 +80,6 @@ export class TSDocConfigFile {
         + '\nError in file: ' + jsonFilePath);
     }
 
-    return new TSDocConfigFile({
-      filePath: fullJsonFilePath
-    });
+    return new TSDocConfigFile(fullJsonFilePath, configJson);
   }
 }
--- tsdoc-config/src/__tests__/ConfigLoader.test.ts
+++ tsdoc-config/src/__tests__/ConfigLoader.test.ts
@@ -4,30 +4,75 @@ import { TSDocConfigFile } from '../TSDocConfigFile';
 import { ConfigLoader } from '../ConfigLoader';
 
 function getRelativePath(testPath: string): string {
-  return path.relative(__dirname, testPath).split('\\').join('/');
+  return path
+    .relative(__dirname, testPath)
+    .split('\\')
+    .join('/');
 }
 
-test('Resolve p1', () => {
+test('Load p1', () => {
   const configFile: TSDocConfigFile | undefined = ConfigLoader.tryLoadFromPackageFolder(
-    path.join(__dirname, 'assets/p1'));
+    path.join(__dirname, 'assets/p1')
+  );
   expect(configFile).toBeDefined();
   expect(getRelativePath(configFile!.filePath)).toEqual('assets/p1/folder/tsdoc-config.json');
 });
-test('Resolve p2', () => {
+test('Load p2', () => {
   const configFile: TSDocConfigFile | undefined = ConfigLoader.tryLoadFromPackageFolder(
-    path.join(__dirname, 'assets/p2'));
+    path.join(__dirname, 'assets/p2')
+  );
   expect(configFile).toBeDefined();
   expect(getRelativePath(configFile!.filePath)).toEqual('assets/p2/folder/tsdoc-config.json');
 });
-test('Resolve p3', () => {
+test('Load p3', () => {
   const configFile: TSDocConfigFile | undefined = ConfigLoader.tryLoadFromPackageFolder(
-    path.join(__dirname, 'assets/p3'));
+    path.join(__dirname, 'assets/p3')
+  );
   expect(configFile).toBeDefined();
   expect(getRelativePath(configFile!.filePath)).toEqual('assets/p3/folder/tsdoc-config.json');
 });
-test('Resolve p4', () => {
+test('Load p4', () => {
   const configFile: TSDocConfigFile | undefined = ConfigLoader.tryLoadFromPackageFolder(
-    path.join(__dirname, 'assets/p4'));
+    path.join(__dirname, 'assets/p4')
+  );
   expect(configFile).toBeDefined();
   expect(getRelativePath(configFile!.filePath)).toEqual('assets/p4/tsdoc-config.json');
+});
+test('Load p5', () => {
+  const configFile: TSDocConfigFile | undefined = ConfigLoader.tryLoadFromPackageFolder(
+    path.join(__dirname, 'assets/p5')
+  );
+  expect(configFile).toMatchInlineSnapshot(
+    { filePath: expect.any(String) },
+    `
+    Object {
+      "extends": Array [],
+      "filePath": Any<String>,
+      "tagDefinitions": Array [
+        TSDocTagDefinition {
+          "allowMultiple": true,
+          "standardization": "None",
+          "syntaxKind": 0,
+          "tagName": "@myInlineTag",
+          "tagNameWithUpperCase": "@MYINLINETAG",
+        },
+        TSDocTagDefinition {
+          "allowMultiple": false,
+          "standardization": "None",
+          "syntaxKind": 1,
+          "tagName": "@myBlockTag",
+          "tagNameWithUpperCase": "@MYBLOCKTAG",
+        },
+        TSDocTagDefinition {
+          "allowMultiple": false,
+          "standardization": "None",
+          "syntaxKind": 2,
+          "tagName": "@myModifierTag",
+          "tagNameWithUpperCase": "@MYMODIFIERTAG",
+        },
+      ],
+      "tsdocVersion": "0.12",
+    }
+  `
+  );
 });
