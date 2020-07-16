9aea179474158f7174fca76e42639a37e282f316
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2020-07-15 22:26:35 -0700

    Work in progress converting playground to invoke webpack using Heft

--- playground/build.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const productionIndex = process.argv.indexOf('--production');
-if (productionIndex !== -1) {
-  process.argv[productionIndex] = '--env.production';
-}
-
-require('webpack-cli/bin/cli');
