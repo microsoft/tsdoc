ec8993e2ac68e7588867cad50b513583f3cdb8f9
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2020-11-24 19:40:56 -0800

    Update .prettierrc.js based on Rush website example

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,10 +3,13 @@ module.exports = {
   // We use a larger print width because Prettier's word-wrapping seems to be tuned
   // for plain JavaScript without type annotations
   printWidth: 110,
-  // Microsoft style quotes
-  singleQuote: true,
-  // Preserve existing newlines
+
+  // Use .gitattributes to manage newlines
   endOfLine: 'auto',
+
+  // Use single quotes instead of double quotes
+  singleQuote: true,
+
   // For ES5, trailing commas cannot be used in function parameters; it is counterintuitive
   // to use them for arrays only
   trailingComma: 'none'
