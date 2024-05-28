f73132ee9f45134606e816fc58ccd62ed23986ec
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2024-05-28 00:59:48 -0700

    Eliminate eslint-config and typescript dependencies in most projects.

--- tsdoc/.eslintrc.js
+++ tsdoc/.eslintrc.js
@@ -1,8 +1,13 @@
 // This is a workaround for https://github.com/eslint/eslint/issues/3458
-require('@rushstack/eslint-config/patch/modern-module-resolution');
+require('@rushstack/heft-web-rig/profiles/library/includes/eslint/patch/modern-module-resolution');
+// This is a workaround for https://github.com/microsoft/rushstack/issues/3021
+require('@rushstack/heft-web-rig/profiles/library/includes/eslint/patch/custom-config-package-names');
 
 module.exports = {
-  extends: ['@rushstack/eslint-config/profile/web-app', '@rushstack/eslint-config/mixins/friendly-locals'],
+  extends: [
+    '@rushstack/heft-web-rig/profiles/library/includes/eslint/profile/web-app',
+    '@rushstack/heft-web-rig/profiles/library/includes/eslint/mixins/friendly-locals'
+  ],
   parserOptions: { tsconfigRootDir: __dirname },
 
   plugins: ['eslint-plugin-header'],
