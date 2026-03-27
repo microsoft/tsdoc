93bb4ad27d19d5ae2f1ad9fdfda6e5edcd4c4a41
Zendy <50132805+zendy199x@users.noreply.github.com>
2026-03-27 10:57:30 +0700

    feat: create recommended eslint configuration
    Create a new configuration file that defines the `recommended` settings for `eslint-plugin-tsdoc`. This configuration will enable the `tsdoc/syntax` rule with a `warn` severity, allowing users to easily adopt TSDoc validation by extending `plugin:tsdoc/recommended` in their ESLint setup.
    
    Signed-off-by: Zendy <50132805+zendy199x@users.noreply.github.com>

--- eslint-plugin/src/index.ts
+++ eslint-plugin/src/index.ts
@@ -9,6 +9,7 @@ import type { TSDocConfigFile } from '@microsoft/tsdoc-config';
 
 import { Debug } from './Debug';
 import { ConfigCache } from './ConfigCache';
+import recommended from './configs/recommended';
 
 const tsdocMessageIds: { [x: string]: string } = {};
 
@@ -19,6 +20,7 @@ defaultTSDocConfiguration.allTsdocMessageIds.forEach((messageId: string) => {
 
 interface IPlugin {
   rules: { [x: string]: eslint.Rule.RuleModule };
+  configs: { [x: string]: eslint.Linter.Config };
 }
 
 function getRootDirectoryFromContext(context: TSESLint.RuleContext<string, unknown[]>): string | undefined {
@@ -68,94 +70,3 @@ const plugin: IPlugin = {
         const tsConfigDir: string | undefined = getRootDirectoryFromContext(
           context as unknown as TSESLint.RuleContext<string, unknown[]>
         );
-        Debug.log(`Linting: "${sourceFilePath}"`);
-
-        const tsdocConfiguration: TSDocConfiguration = new TSDocConfiguration();
-
-        try {
-          const tsdocConfigFile: TSDocConfigFile = ConfigCache.getForSourceFile(sourceFilePath, tsConfigDir);
-          if (!tsdocConfigFile.fileNotFound) {
-            if (tsdocConfigFile.hasErrors) {
-              context.report({
-                loc: { line: 1, column: 1 },
-                messageId: 'error-loading-config-file',
-                data: {
-                  details: tsdocConfigFile.getErrorSummary()
-                }
-              });
-            }
-
-            try {
-              tsdocConfigFile.configureParser(tsdocConfiguration);
-            } catch (e) {
-              context.report({
-                loc: { line: 1, column: 1 },
-                messageId: 'error-applying-config',
-                data: {
-                  details: e.message
-                }
-              });
-            }
-          }
-        } catch (e) {
-          context.report({
-            loc: { line: 1, column: 1 },
-            messageId: 'error-loading-config-file',
-            data: {
-              details: `Unexpected exception: ${e.message}`
-            }
-          });
-        }
-
-        const tsdocParser: TSDocParser = new TSDocParser(tsdocConfiguration);
-
-        const sourceCode: eslint.SourceCode = context.sourceCode ?? context.getSourceCode();
-        function checkCommentBlocks(): void {
-          for (const comment of sourceCode.getAllComments()) {
-            if (comment.type !== 'Block') {
-              continue;
-            }
-            if (!comment.range) {
-              continue;
-            }
-
-            const textRange: TextRange = TextRange.fromStringRange(
-              sourceCode.text,
-              comment.range[0],
-              comment.range[1]
-            );
-
-            // Smallest comment is "/***/"
-            if (textRange.length < 5) {
-              continue;
-            }
-            // Make sure it starts with "/**"
-            if (textRange.buffer[textRange.pos + 2] !== '*') {
-              continue;
-            }
-
-            const parserContext: ParserContext = tsdocParser.parseRange(textRange);
-            for (const message of parserContext.log.messages) {
-              context.report({
-                loc: {
-                  start: sourceCode.getLocFromIndex(message.textRange.pos),
-                  end: sourceCode.getLocFromIndex(message.textRange.end)
-                },
-                messageId: message.messageId,
-                data: {
-                  unformattedText: message.unformattedText
-                }
-              });
-            }
-          }
-        }
-
-        return {
-          Program: checkCommentBlocks
-        };
-      }
-    }
-  }
-};
-
-export = plugin;
